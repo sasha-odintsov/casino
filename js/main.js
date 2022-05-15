4de62cb258a75a036a7817457372e65fc57b04ec
Sasha <aleks-one@ukr.net>
2022-05-15 15:01:08 +0300

    Implement point solution

--- js/main.js
+++ js/main.js
@@ -17,123 +17,17 @@ let rolledPoint = 0;
 
 document.querySelector("#balance").innerHTML = balance + "$";
 
-//----without point----
-function rollDice() { 
-    let cube1 = getRandomInt(1, 6);
-    let cube2 = getRandomInt(1, 6);    
-    let betWin = document.getElementById("bet").value * 2;
-    let bet = document.getElementById("bet").value;    
-    document.getElementById("balance-result").innerHTML = balance + "$";
-    if (getResultDice(cube1, cube2) === "win") {
-        layout("bet-result");
-        document.getElementById("result-text").innerHTML = `Вы выграли, и получаете <strong>${betWin}$</strong>`;
-        balance = balance + betWin;
-        document.getElementById("balance").innerHTML = balance + "$";
-    } else {
-        layout("bet-result");
-        document.getElementById("result-text").innerHTML = `Ваша ставка в <strong>${bet}$</strong> не сыграла`;
-        balance = balance - bet;
-        document.getElementById("balance").innerHTML = balance + "$";
-    }
-    document.getElementById("dice-first-result").innerHTML = cube1;
-    document.getElementById("dice-second-result").innerHTML = cube2;
-}
+// ******with point******//
 
-function getResultDice (a, b) { 
+function getResultDice(a, b) { 
     let sum = a + b;
-    return (sum === 7 || sum === 11) ? "win" : "lose";
-}
-
-// function getResultDice(a, b) { 
-//     let sum = a + b;
-//     if (sum === 7 || sum === 11) {
-//         return "win";
-//     } else  {
-//         return "lose";
-//     } 
-// }
-
-//----point try------
-// function rollDice() {
-    // let cube1 = getRandomInt(1, 6);
-    // let cube2 = getRandomInt(1, 6);
-    // let sum = cube1 + cube2;
-    // let betWin = document.getElementById("bet").value * 2;
-    // let bet = document.getElementById("bet").value;
-    // let point = getResultPoint(cube1, cube2, rolledPoint);
-    // document.getElementById("balance-result").innerHTML = balance + "$";
-    // if (getResultDice(cube1, cube2) === "win") {
-    //     layout("bet-result");
-    //     document.getElementById("result-text").innerHTML = `Вы выграли, и получаете <strong>${betWin}$</strong>`;
-    //     balance = balance + betWin;
-    //     document.getElementById("balance").innerHTML = balance + "$";
-    // } else if (getResultDice(cube1, cube2) === "lose") {
-    //     layout("bet-result");
-    //     document.getElementById("result-text").innerHTML = `Ваша ставка в <strong>${bet}$</strong> не сыграла`;
-    //     balance = balance - bet;
-    //     document.getElementById("balance").innerHTML = balance + "$";
-    // } else {
-    //     layout("bet-point");
-    //     document.getElementById("bet-amound").innerHTML = bet + "$";
-    //     document.getElementById("point").innerHTML = sum;
-    //     document.getElementById("dice-first-point").innerHTML = cube1;
-    //     document.getElementById("dice-second-point").innerHTML = cube2;
-
-
-        //-----------------------
-        // rolledPoint === sum;
-        // if (rolledPoint) { 
-        //     getResultPoint(cube1, cube2, rolledPoint) === getResultDice(cube1, cube2);
-        // }
-
-        
-
-
-        // getResultPoint(cube1, cube2, rolledPoint, balance, bet, betWin);
-        //-----------------------
-        
-        
-        // if (point === "lose") {
-        //     document.querySelector(".point-txt").innerHTML = `Вы проиграли <strong>${bet}$</strong>`
-        //     balance = balance - bet;
-        // } else if (point === "win") {
-        //     document.querySelector(".point-txt").innerHTML = `Вы выграли <strong>${betWin}$</strong>`
-        //     balance = balance + betWin;
-        // } else { 
-        //     document.getElementById("result-text").innerHTML = `Твое очко - <strong>${sum}</strong> <br> Продолжай бросать`;
-        // }
-       
-        
-
-        // if (sum === 7) {
-        //     document.querySelector(".point-txt").innerHTML = `Вы проиграли <strong>${bet}$</strong>`
-        //     balance = balance - bet;
-        // } else if (sum === sum) {
-        //     document.querySelector(".point-txt").innerHTML = `Вы выграли <strong>${betWin}$</strong>`
-        //     balance = balance + betWin;
-        // }
-
-        // document.getElementById("result-text").innerHTML = `Твое очко - <strong>${sum}</strong> <br> Продолжай бросать`;
-//     }
-//     document.getElementById("dice-first-result").innerHTML = cube1;
-//     document.getElementById("dice-second-result").innerHTML = cube2;
-// }
-
-
-//--------with point---------
-// function getResultDice(a, b) { 
-//     let sum = a + b;
-//     if (sum === 7 || sum === 11) {
-//         return "win";
-//     } else if (sum === 2 || sum === 8 || sum === 12) {
-//         return "lose";
-//     } else { 
-//         return "point";
-//     }
-// }
-
-function tryMore() {
-    layout("bet-form");
+    if (sum === 7 || sum === 11) {
+        return "win";
+    } else if (sum === 2 || sum === 8 || sum === 12) {
+        return "lose";
+    } else { 
+        return "point";
+    }
 }
 
 function getResultPoint(a, b, point) {
@@ -146,3 +40,79 @@ function getResultPoint(a, b, point) {
         return "point";        
     }
 }
+
+function tryMore() {
+    layout("bet-form");
+}
+
+function rollDice() {
+    let cube1 = getRandomInt(1, 6);
+    let cube2 = getRandomInt(1, 6);
+    let sum = cube1 + cube2;
+    let decision = getResultDice(cube1, cube2);
+    let bet = document.getElementById("bet").value;
+    let betWin = bet * 2;
+    document.getElementById("balance-result").innerHTML = balance + "$";
+    document.getElementById("dice-first-result").innerHTML = cube1;
+    document.getElementById("dice-second-result").innerHTML = cube2;
+
+    if (rolledPoint) {
+        decision = getResultPoint(cube1, cube2, rolledPoint);
+    }
+
+    if (decision === "point") {
+        if (!rolledPoint) {
+            rolledPoint = sum;
+        }; 
+        layout("bet-point");
+        document.getElementById("bet-amound").innerHTML = bet + "$";
+        document.getElementById("point").innerHTML =  rolledPoint;
+        document.getElementById("dice-first-point").innerHTML = cube1;
+        document.getElementById("dice-second-point").innerHTML = cube2;   
+    } else {
+        layout("bet-result");
+        rolledPoint = 0;
+        if (decision === "win") {
+            document.getElementById("result-text").innerHTML = `Вы выграли, и получаете <strong>${betWin}$</strong>`;
+            balance = balance + betWin;
+        } else {
+            document.getElementById("result-text").innerHTML = `Ваша ставка в <strong>${bet}$</strong> не сыграла`;
+            balance = balance - bet;
+        }
+        document.getElementById("balance").innerHTML = balance + "$";
+        document.querySelector('.button-result').onclick = tryMore;
+    } 
+}
+
+//*****without point*****//
+
+// function rollDice() { 
+//     let cube1 = getRandomInt(1, 6);
+//     let cube2 = getRandomInt(1, 6);    
+//     let betWin = document.getElementById("bet").value * 2;
+//     let bet = document.getElementById("bet").value;    
+//     document.getElementById("balance-result").innerHTML = balance + "$";
+//     document.getElementById("dice-first-result").innerHTML = cube1;
+//     document.getElementById("dice-second-result").innerHTML = cube2;
+//     layout("bet-result");
+//     if (getResultDice(cube1, cube2) === "win") {
+//         document.getElementById("result-text").innerHTML = `Вы выграли, и получаете <strong>${betWin}$</strong>`;
+//         balance = balance + betWin;    
+//     } else {   
+//         document.getElementById("result-text").innerHTML = `Ваша ставка в <strong>${bet}$</strong> не сыграла`;
+//         balance = balance - bet;
+//     }
+//     document.getElementById("balance").innerHTML = balance + "$";
+//     document.querySelector('.button-result').addEventListener('click', function(){
+//         tryMore();
+//     })
+// }
+
+// function getResultDice (a, b) { 
+//     let sum = a + b;
+//     return (sum === 7 || sum === 11) ? "win" : "lose";
+// }
+
+// function tryMore() {
+//     layout("bet-form");
+// }
